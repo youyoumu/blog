af18fe7e75ad49a8724986e9e1eef27e6f2eb8e7
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-02-02 14:42:51 +0700

    Allow app.youyoumu.my.id host address

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Blog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.hosts << "app.youyoumu.my.id"
   end
 end
